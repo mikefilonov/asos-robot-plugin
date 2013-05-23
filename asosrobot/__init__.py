b9979aba28421198d2ce182d2fea0cb7e5eb2915
Linux User <mikefilonov@gmail.com>
2013-05-23 19:42:43 +0000

    added asos.catch plugin

--- asosrobot/__init__.py
+++ asosrobot/__init__.py
@@ -1,5 +1,9 @@
 from productdetails import AsosProductDetailsJob
+from catchproduct import AsosCatchProductJob
+
 
 def register_plugin( pm ):
 	pm.register("details.asos", AsosProductDetailsJob)
+	pm.register("catch.asos", AsosCatchProductJob)
+
 	
